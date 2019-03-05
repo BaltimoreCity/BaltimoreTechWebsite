589e1edc487ae9c83c792974575f6082e9f43d40
maysam <kherad@gmail.com>
2019-03-05 23:55:17 +0330

    changing peoples.js to use people data from subfolder structure

--- assets/js/peoples.js
+++ assets/js/peoples.js
@@ -19,7 +19,8 @@ function initSearch() {
 
 function initPagination() {
   const peoples = [
-    {% for people_hash in site.data.peoples %}
+    {% for people_hash2 in site.data.peoples %}
+    {% for people_hash in people_hash2[1] %}
     {% assign people = people_hash[1] %}
       {
         name: `{{people.name}}`,
@@ -27,9 +28,15 @@ function initPagination() {
         profession: `{{people.profession}}`,
         education: `{{people.education}}`,
         skills: {{people.skills | jsonify}},
-        image_url: `{{people.picture | prepend: site.baseurl}}`,
+        image_url: `{{people.picture}}`,
         resume_url: `{{resume.url}}`,
-      }{% unless forloop.last %},{% endunless %}
+        twitter_url: `{{people.links.twitter}}`,
+        facebook_url: `{{people.links.facebook}}`,
+        linkedin_url: `{{people.links.linkedin}}`,
+        github_url: `{{people.links.github}}`,
+        website_url: `{{people.links.website}}`,
+      },
+    {% endfor %}
     {% endfor %}
   ];
 
@@ -48,6 +55,7 @@ function template(data) {
   let html = '';
 
   $.each(data, function(index, people) {
+
     const peopleHtml = `
       <div class="col-sm-11 people-card ${people.type}">
         <div class="row">
@@ -71,14 +79,29 @@ function template(data) {
                 ></a>
               </li>
               <li class="list-inline-item">
-                <a href="https://twitter.com" target="_blank"
+                <a href="${people.twitter_url}" target="_blank"
                   ><i class="fab fa-twitter"></i
                 ></a>
               </li>
               <li class="list-inline-item">
-                <a href="https://linkedin.com" target="_blank"
+                <a href="${people.linkedin_url}" target="_blank"
                   ><i class="fab fa-linkedin-in"></i
                 ></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="${people.facebook_url}" target="_blank"
+                  ><i class="fab fa-facebook"></i
+                ></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="${people.github_url}" target="_blank"
+                  ><i class="fab fa-github-alt"></i
+                ></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="${people.website_url}" target="_blank">
+                  <i class="fa fa-link"></i>
+                </a>
               </li>
             </ul>
             <a
