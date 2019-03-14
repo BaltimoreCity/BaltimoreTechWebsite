c87bd88f23dbd035f75fb0b6889f98ce1e3dfe68
maysam <kherad@gmail.com>
2019-03-14 23:02:53 +0330

    remove debug lines

--- grab_events.rb
+++ grab_events.rb
@@ -8,15 +8,13 @@ TOKEN = "4a2f9464b2749587b6c26557a59268"
 
 meetup_folder = '_data/meetups/'
 events_folder = '_data/events/'
-p meetup_folder
 Dir.entries(meetup_folder).select {|f| !File.directory? f}.each do |meetup_file|
-  p meetup_file
   file = "#{meetup_folder}#{meetup_file}"
   data = YAML::load open(file)
   url = data['url']
   usable = url.split(/www.meetup.com|\//).last
   source = "https://api.meetup.com/#{usable}/events/?key=#{TOKEN}"
-  p source
+
   raw = open(source).read
   data = JSON.parse(raw)
   data.each do |event|
@@ -24,7 +22,6 @@ Dir.entries(meetup_folder).select {|f| !File.directory? f}.each do |meetup_file|
     date = Date.parse event['local_date']
     year = date.year
     month = date.month
-    p slug
     FileUtils.mkdir_p("#{events_folder}#{year}")
     FileUtils.mkdir_p("#{events_folder}#{year}/#{month}")
     File.open("#{events_folder}#{year}/#{month}/#{slug}.yml", "w") { |file| file.write(event.to_yaml)}
